408183e8d268280a54298ceeb4e9c2e06afffc9a
Chelsea Smith <smith2chelsea2@gmail.com>
2021-05-16 01:18:07 -0400

    color change function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,17 @@
+// current day
 var today = moment ();
 $("#today-date").text(today.format("MMM Do, YYYY"));
 
-var hour = moment
+var time = $(".hour");
+var now = parseInt(moment().format("H"));
+
+function hour () {
+    var hourEl = parseInt($this).attr("#id");
+    if (hourEl === now){
+        $(slot).addClass("current");
+    } else if (hourEl < now){
+        $(slot).addClass("past");
+    } else if (hourEl > now){
+        $(slot).addClass("upcoming")
+    };
+}
